685148a82ccb037ee34d48835dad7ad1ba5b4ace
saba-ramezani <saba.rad.sr@gmail.com>
2025-05-19 13:57:40 +0330

    initialized project and added carousel

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,14 @@
+import Carousel from "./Carousel"
+
+const HomePage = () => {
+  return (
+    <div className="bg-DEFAULT">
+      <div className="min-w-[1000px] max-w-[1500px] m-auto bg-purple-400">
+        
+        <Carousel />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage

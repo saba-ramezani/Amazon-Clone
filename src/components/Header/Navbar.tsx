db7aee18cd13ae18115ba1132d86c9c5c284ed7f
saba-ramezani <saba.rad.sr@gmail.com>
2025-05-25 13:39:52 +0330

    updated links of navbar

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -2,6 +2,7 @@ import { IoLocationOutline } from "react-icons/io5";
 import { GrLanguage } from "react-icons/gr";
 import { FaCartShopping } from "react-icons/fa6";
 import Search from "./Search";
+import { Link } from "react-router-dom";
 
 
 
@@ -9,7 +10,10 @@ const Navbar = () => {
   return (
     <div className='w-full flex h-[70px] bg-DEFAULT px-[20px] gap-[25px]'>
         <div className='w-fit h-full flex items-center  hover:scale-[105%]'>
-            <img className='h-[40px] object-cover' src="/images/amazon.png" alt="" />
+            <Link to={'/'}>
+                <img className='h-[40px] object-cover' src="/images/amazon.png" alt="" />
+            </Link>
+            
         </div>
         <div className="flex flex-col h-full items-start justify-center hover:scale-[105%]">
             <p className="text-gray-400 text-[13px] ml-[20px] leading-4">Deliver to</p>
@@ -37,10 +41,13 @@ const Navbar = () => {
                 <p className="text-white text-[15px] font-semibold">& Orders</p>
             </div>
         </div>
-        <div className="flex h-full items-center justify-center gap-1  hover:scale-[105%]">
-            <FaCartShopping  size={40} color="Orange" />
-            <p className="text-white text-[16px] font-semibold mt-[20px]">Cart</p>
-        </div>
+        <Link to={"/checkout"}>
+            <div className="flex h-full items-center justify-center gap-1  hover:scale-[105%]">
+                <FaCartShopping  size={40} color="Orange" />
+                <p className="text-white text-[16px] font-semibold mt-[20px]">Cart</p>
+            </div>
+        </Link>
+        
     </div>
   )
 }

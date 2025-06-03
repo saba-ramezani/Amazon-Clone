d649d28e281d88b28a3eeb7e2ff4d3731c999de7
saba-ramezani <saba.rad.sr@gmail.com>
2025-06-03 10:56:18 +0330

    added initial setup of redux for managing cart

---
+++ src/redux/cartSlice.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    prodcuts: [],
+    productsNumber: 0
+}
+
+export const cartSlice = createSlice({
+    name: "cart",
+    initialState,
+    reducers: {
+        addToCart: (state, action) => {
+
+        },
+        removeFromCart: (state, action) => {
+
+        }
+    }
+})
+
+
+export const {addToCart, removeFromCart} = cartSlice.actions;
+export default cartSlice.reducer;
